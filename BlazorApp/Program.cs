28b0572fc64672394124a61796ac95a409678f31
Michael Gfeller <michael.gfeller@ost.ch>
2023-07-25 13:49:09 +0200

    init

---
+++ BlazorApp/Program.cs
@@ -0,0 +1,40 @@
+using BlazorApp;
+using BlazorApp.Services;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace BlazorApp
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped<DataService, DataService>();
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddBlazoredLocalStorageAsSingleton(config =>
+            {
+                config.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+                config.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+                config.JsonSerializerOptions.IgnoreReadOnlyProperties = true;
+                config.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+                config.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+                config.JsonSerializerOptions.ReadCommentHandling = JsonCommentHandling.Skip;
+                config.JsonSerializerOptions.WriteIndented = false;
+            }); 
+
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ BlazorApp/Services/DataService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Net.Http.Json;
+using static BlazorApp.Pages.FetchData;
+using static System.Net.WebRequestMethods;
+
+namespace BlazorApp.Services
+{
+    public class DataService
+    {
+        private readonly HttpClient http;
+
+        public DataService(HttpClient http) {
+            this.http = http;
+        }    
+        public async Task<WeatherForecast[]> GetData()
+        {
+            return await http.GetFromJsonAsync<WeatherForecast[]>("sample-data/weather.json");
+        }
+    }
+}
